e9bf48126d4c3bcf990299801e3656153b0e89a0
Mauro Bianchin <142336896+exodia-info@users.noreply.github.com>
2024-08-26 20:50:52 +0200

    Update layout.js
    ciap

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,11 @@ export default function RootLayout({ children, className, customStyle }) {
           rel="stylesheet"
           href="https://use.typekit.net/uty0gas.css"
         ></link>
+                    <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3185380127106627"
+          crossorigin="anonymous"
+        ></script>
       </head>
       <body className={className}>
         <ThemeProvider>
